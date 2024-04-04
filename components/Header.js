91105996d4199692a642fea984697f71ac01b238
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2024-04-03 17:41:27 -0700

    added particles

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,18 @@
+// next image
+import Image from "next/image";
+
+// next link
+import Link from "next/link";
+
+// components
+import Socials from "../components/Socials";
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">header</div>
+    </header>
+  );
 };
 
 export default Header;
